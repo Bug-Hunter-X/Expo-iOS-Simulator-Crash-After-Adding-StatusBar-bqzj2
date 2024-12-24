8e798a884a34031035de7302a0636b9300acfa96
BugHunter-X <askzenai@gmail.com>
2024-12-24 16:58:07 +0100

    Add App.js file

---
+++ App.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, SafeAreaView, StatusBar } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle={'dark-content'} backgroundColor={'#fff'} />
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+});
